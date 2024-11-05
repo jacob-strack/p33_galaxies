54f81e644b4adbd9e63c19137a210fdc705ba8d6
dcollins <dcollins4096@gmail.com>
2024-11-05 12:09:02 -0500

    Slew camera around zone

--- maker2.py
+++ maker2.py
@@ -29,13 +29,37 @@ if 1:
     #proj_axis=nar([0,1,1],dtype='float')
     #proj_axis/=(proj_axis**2).sum()
 
-    theta = np.linspace(np.pi/7,np.pi*5/6,10)-np.pi/2
-    phi   = np.linspace(-np.pi,np.pi,10)
-    for ph in phi:
-        for th in theta:
-            proj_axis=nar(astropy.coordinates.spherical_to_cartesian(1,th,ph))
-            print(proj_axis)
-            corners=s2p.project(cube,xyz,dxyz,proj_center,proj_axis)
+    if 1:
+        #slew the camera around the zone
+        theta = np.linspace(np.pi/7,np.pi*5/6,10)-np.pi/2
+        phi   = np.linspace(-np.pi,np.pi,10)
+        theta = theta[5:6]
+        phi = phi[1:2]
+        r=2
+        for nph,ph in enumerate(phi):
+            for nth,th in enumerate(theta):
+                print(nph,nth)
+                x=r*np.sin(th)*np.cos(ph)
+                y=r*np.sin(th)*np.sin(ph)
+                z=r*np.cos(th)
+                proj_center=nar([x,y,z])
+                dcenter = proj_center-new_center.flatten()
+                proj_axis = -dcenter/(dcenter**2).sum()
+                bucket={'theta':th,'phi':ph}
+                corners=s2p.project(cube,xyz,dxyz,proj_center,proj_axis, bucket=bucket)
+
+
+
+    if 0:
+        #rotate the camera, fixed in space
+        theta = np.linspace(np.pi/7,np.pi*5/6,10)-np.pi/2
+        phi   = np.linspace(-np.pi,np.pi,10)
+        for ph in phi:
+            for th in theta:
+                proj_axis=nar(astropy.coordinates.spherical_to_cartesian(1,th,ph))
+                print(proj_axis)
+                bucket={'theta':th,'phi':ph}
+                corners=s2p.project(cube,xyz,dxyz,proj_center,proj_axis, bucket=bucket)
 
 if 0:
     wmap_map_I = hp.read_map("wmap_band_iqumap_r9_7yr_W_v4.fits")
