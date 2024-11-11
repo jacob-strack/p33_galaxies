40eed4859629c8edaa1719062c6bf8c65e62425f
dcollins <dcollins4096@gmail.com>
2024-11-11 13:00:51 -0500

    Draft 1.1, galaxy

--- maker3.py
+++ maker3.py
@@ -10,7 +10,7 @@ reload(proj)
 
 if 1:
     #strafe the camera around the zone
-    cube, xyz, dxyz = proj.make_cube_full(32, stick_or_sphere=1)
+    cube, xyz, dxyz = proj.make_cube_full(32, stick_or_sphere=0)
     molplot=False
     theta = np.linspace(np.pi/7,np.pi*5/6,10)-np.pi/2
     phi   = np.linspace(-np.pi,np.pi,10)
@@ -31,7 +31,7 @@ if 1:
             proj_axis = -dcenter/(dcenter**2).sum()
             bucket={'theta':th,'phi':ph}
 
-            image=s2p.project(cube,xyz,dxyz,proj_center,proj_axis, bucket=bucket,molplot=molplot, NSIDE=64)
+            image=s2p.project(cube,xyz,dxyz,proj_center,proj_axis, bucket=bucket,molplot=molplot, NSIDE=128)
 
             plt.clf()
             hp.mollview(image, title="Two Sticks")
@@ -39,6 +39,52 @@ if 1:
             nplots = len(glob.glob(prefix+"*"))
             plt.savefig(prefix+"%03d"%nplots)
 
+
+if 0:
+    ds = yt.load('datasets/IsolatedGalaxy/galaxy0030/galaxy0030')
+    #ad = ds.all_data()
+    c = ds.arr([0.5]*3, 'code_length')
+    r = ds.quan(1./16, 'code_length')
+    L = ds.arr([0.5-1./32]*3,'code_length')
+    R = ds.arr([0.5+1./32]*3,'code_length')
+    dx = ds.index.get_smallest_dx()*16
+    Nz = (R-L)/dx
+    cg = ds.covering_grid(4,L,Nz)
+    ad=cg
+    #sphere = ds.sphere(c, r)
+    cube = ad['density'].in_units('code_density').flatten().v
+    space_units='code_length'
+    xyz = np.stack([ad['x'].in_units(space_units).v.flatten(),
+                    ad['y'].in_units(space_units).v.flatten(),
+                    ad['z'].in_units(space_units).v.flatten()])
+    dxyz = np.stack([ad['dx'].in_units(space_units).flatten().v,
+                     ad['dy'].in_units(space_units).flatten().v,
+                     ad['dz'].in_units(space_units).flatten().v])
+    if 0:
+        density=cg['density']
+        print("Nz %0.2e"%density.size)
+        fig,ax=plt.subplots(1,1)
+        ax.imshow( np.log(density.sum(axis=2)))
+        fig.savefig("%s/galaxy_cg"%plot_dir)
+
+    if 0:
+        proj=ds.proj('density',2, data_source=sphere)
+        pw=proj.to_pw()
+        #pw.zoom(16)
+        pw.save('%s/galaxy'%plot_dir)
+
+if 1:
+    dx_min = dxyz.min()
+
+    proj_center = np.array([0.5]*3)+np.array([0,0,-1./32]) + dx_min/128
+    proj_axis = np.array([0,0,1.])
+    image=s2p.project(cube,xyz,dxyz,proj_center,proj_axis, bucket=bucket,molplot=molplot, NSIDE=128)
+
+    plt.clf()
+    hp.mollview(image, title="Isolated Galaxy")
+    prefix='%s/galaxy'%plot_dir
+    nplots = len(glob.glob(prefix+"*"))
+    plt.savefig(prefix+"%03d"%nplots)
 
 
 if 0:
